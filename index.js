a8125365b496ccc2250da97c0e6856dc4e50d293
ypradeeprao <39065049+ypradeeprao@users.noreply.github.com>
2023-02-02 01:04:11 +0000

    dfd

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const express = require('express')
 const app = express()
+
+var fs = require("fs");
+
+app.get('/listUsers', function (req, res) {
+   fs.readFile("/" + "users.json", 'utf8', function (err, data) {
+      console.log( data );
+      res.end( data );
+   });
+})
+
+
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yooo!')
