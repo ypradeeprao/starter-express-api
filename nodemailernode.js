8ca46828be3921a0d08531dbabe3b254690e8be4
ypradeeprao <39065049+ypradeeprao@users.noreply.github.com>
2023-02-04 04:48:28 +0000

    fdfdfd

--- nodemailernode.js
+++ nodemailernode.js
@@ -3,14 +3,14 @@ var nodemailer = require('nodemailer');
 var transporter = nodemailer.createTransport({
   service: 'Gmail',
   auth: {
-    user: 'ypradeeprao@gmail.com',
-    pass: 'Tir6tw3q@@'
+    user: 'admin@cloudreed.com',
+    pass: 'Tir6tw3q@'
   }
 });
 
 const sendmail = async function(req){
 var mailOptions = {
-  from: 'ypradeeprao@gmail.com',
+  from: 'admin@cloudreed.com',
   to: 'pradeepsfdc07@yahoo.com',
   subject: 'Sending Email using Node.js',
   text: 'That was easy!'
