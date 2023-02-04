36dfa462c64a5dc665af60f78ec2e7d21a717f9b
ypradeeprao <39065049+ypradeeprao@users.noreply.github.com>
2023-02-04 05:36:44 +0000

    gfgffdfd

--- twilionode.js
+++ twilionode.js
@@ -6,10 +6,10 @@ const client = require('twilio')(accountSid, authToken);
 
 const sendmobilemessage = async function(req){
 
-    let resp = {issuccess:"true", message:""};
-  
-    let {phonenumber, message} = req.body;
-   console.log(req.body);
+let resp = {issuccess:"true", message:""};
+
+let {phonenumber, message} = req.body;
+console.log(req.body);
 
 await client.messages 
 .create({ 
@@ -17,10 +17,14 @@ body: message,
 messagingServiceSid: 'MG4d433d25ea1ba7870e5cd84bc4b6746d',      
 to: phonenumber 
 }) 
-.then(message => console.log(message.sid)) 
-.done();
+.then(message => {
+    console.log('Sent invite to ' + phonenumber + ' with SID ' + message.sid);
+})
+.catch(error => {
+    console.log(error);
+});
 
 }
 module.exports ={
-    sendmobilemessage
+sendmobilemessage
 }
