2d215592485de22514b7990a9d093f8582ead333
ypradeeprao <39065049+ypradeeprao@users.noreply.github.com>
2023-02-05 04:01:42 +0000

    gfgfgfgffd

--- fileupload.js
+++ fileupload.js
@@ -2,6 +2,7 @@ var http = require('http');
 var formidable = require('formidable');
 
 const fileupload = async function(req){
+    console.log(req.files);
     let resp = {issuccess:"true", message:"cannot be deleted from frontend"};
     var form = new formidable.IncomingForm();
     form.parse(req, function (err, fields, files) {
