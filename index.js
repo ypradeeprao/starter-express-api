689ba3e209ce0a22eb6308a97870ff67f9c7b24e
ypradeeprao <39065049+ypradeeprao@users.noreply.github.com>
2023-02-07 22:30:01 +0000

    gfgfgf

--- index.js
+++ index.js
@@ -11,25 +11,18 @@ const fileuploadjs = require("./fileupload.js")
 
 const bodyParser = require('body-parser');
 app.use(cors());
-app.use((req,res,next)=>{
-    res.header('Access-Control-Allow-Headers, *, Access-Control-Allow-Origin', 'Origin, X-Requested-with, Content_Type,Accept,Authorization','http://localhost:4200');
-    if(req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
-        return res.status(200).json({});
-    }
-    next();
-});
+
 // Configuring body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
 app.use(function (req, res, next) {
+  //Enabling CORS
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-})
-
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    next();
+  });
 
 
 
@@ -59,8 +52,7 @@ app.get('/:id', function (req, res) {
  app.post('/createtable', async (req, res) => {
   console.log("createtabledfd");
    let mongojsresp = await mongojs.createtable(req) ; 
-   res.header('Access-Control-Allow-Headers, *, Access-Control-Allow-Origin', 'Origin, X-Requested-with, Content_Type,Accept,Authorization','https://cdpn.io');
-   
+  
    res.end( JSON.stringify(mongojsresp));
  })
 
