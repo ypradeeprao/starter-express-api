3d8d3e92dc6a560361d531544d46c4549d819cb2
ypradeeprao <39065049+ypradeeprao@users.noreply.github.com>
2023-02-07 21:38:03 +0000

    gfg

--- index.js
+++ index.js
@@ -17,6 +17,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 
+app.use(function(req, res, next) {
+  // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+  });
+
 
 
 
