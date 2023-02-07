babb9fec9c48c82ea605b9f37732238354307d58
ypradeeprao <39065049+ypradeeprao@users.noreply.github.com>
2023-02-07 22:24:04 +0000

    fdfd

--- index.js
+++ index.js
@@ -10,8 +10,15 @@ const fileuploadjs = require("./fileupload.js")
 
 
 const bodyParser = require('body-parser');
-const cors = require('cors');
-app.use(cors({origin:"https://cdpn.io"}));
+app.use(cors());
+app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Headers, *, Access-Control-Allow-Origin', 'Origin, X-Requested-with, Content_Type,Accept,Authorization','http://localhost:4200');
+    if(req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
+        return res.status(200).json({});
+    }
+    next();
+});
 // Configuring body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
