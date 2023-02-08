dfbfd2107929c870c98a708b5635e3420673ed13
ypradeeprao <39065049+ypradeeprao@users.noreply.github.com>
2023-02-08 20:34:23 +0000

    fdfd

--- index.js
+++ index.js
@@ -148,6 +148,7 @@ var app = express()
 app.get('/listusers', function (req, res) {
  // res.json({msg: 'This is CORS-enabled for all origins!'})
 // res.end( {msg: 'This is CORS-enabled for all origins!'} );
+res.send('Yooo!');
 })
 
 app.all('/', (req, res) => {
