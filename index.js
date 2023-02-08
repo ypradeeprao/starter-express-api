003a3918fe6c9a4c6c0927cf0963a7c22c8f80b4
ypradeeprao <39065049+ypradeeprao@users.noreply.github.com>
2023-02-08 19:22:42 +0000

    fgfgf

--- index.js
+++ index.js
@@ -140,8 +140,8 @@ var express = require('express')
 var cors = require('cors')
 var app = express()
 
-app.options('/products/:id', cors()) // enable pre-flight request for DELETE request
-app.del('/products/:id', cors(), function (req, res, next) {
+app.options('/createtable', cors()) // enable pre-flight request for DELETE request
+app.del('/createtable', cors(), function (req, res, next) {
   res.json({msg: 'This is CORS-enabled for all origins!'})
 })
 
