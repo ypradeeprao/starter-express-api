bc8e00b0da915a9d90ef67942c31cf67349bb8fa
ypradeeprao <39065049+ypradeeprao@users.noreply.github.com>
2023-02-08 20:11:02 +0000

    gfgf

--- index.js
+++ index.js
@@ -145,6 +145,11 @@ app.del('/createtable', cors(), function (req, res, next) {
   res.json({msg: 'This is CORS-enabled for all origins!'})
 })
 
+app.get('/listusers', function (req, res) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+  res.end( data );
+})
+
 app.listen(80, function () {
   console.log('CORS-enabled web server listening on port 80')
 })
