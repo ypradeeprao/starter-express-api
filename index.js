b34a78f138e0850d350a4710e38ef88174d1c05b
ypradeeprao <39065049+ypradeeprao@users.noreply.github.com>
2023-02-09 22:23:45 +0000

    fd

--- index.js
+++ index.js
@@ -145,6 +145,6 @@ app.get('/products', function (req, res, next) {
   res.json({msg: 'This is CORS-enabled for all origins!'})
 })
 
-app.listen(80, function () {
+app.listen(3000, function () {
   console.log('CORS-enabled web server listening on port 80')
 })
